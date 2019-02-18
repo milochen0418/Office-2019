e7dd3472bcecdc54b54c9664e479e32b6e9b7348
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-18 21:35:36 +0000

    Add image but there is a CORS problem

--- vr-react-app/src/App.js
+++ vr-react-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { asset, Pano, Text, View, AmbientLight } from 'react-vr';
+import { asset, Pano, Text, View, Image, AmbientLight } from 'react-vr';
 //import Primitives from './Primitives';
 import Penholder from './Penholder';
 
@@ -9,19 +9,31 @@ export default class App extends React.Component {
       <View>
         <AmbientLight intensity={ 2.5 } />
         <Pano source={asset('background.jpeg')} />
+
+        <Image 
+        crossOrigin="anonymous"
+        source={{uri: 'http://dexon.pieapple.com:8765/images/index.jpg'}} 
+        />        
+
+
         <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [-6, 2, -6]}],
-          }}>
-          VR Beauty
-        </Text>
+         style={{
+           backgroundColor: '#777879',
+           fontSize: 0.8,
+           fontWeight: '400',
+           paddingLeft: 0.2,
+           paddingRight: 0.2,
+           textAlign: 'center',
+           textAlignVertical: 'center',
+           transform: [{translate: [-6, 2, -6]}],
+         }}>
+         VR Beauty
+       </Text>
+
+    
+    
+
+
         <Penholder />
       </View>
     );
